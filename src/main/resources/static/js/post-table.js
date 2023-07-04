34a3e6eb3e55325c9f0d85dc9eeeece4d55e80b9
ch-yang1273 <ych1273@gmail.com>
2023-07-04 18:38:44 +0900

    feat: Post List 테이블에 PostLike 추가

--- src/main/resources/static/js/post-table.js
+++ src/main/resources/static/js/post-table.js
@@ -15,6 +15,11 @@ $(function () {
                     const date = new Date(item.postCreateTime)
                     const link = '/post/view/' + item.postId;
                     const titleLink = '<a href="' + link + '">' + item.title + '</a>';
+
+                    const iconClass = item.isLike ? 'fa-solid' : 'fa-regular';
+                    const icon = $('<i></i>').addClass('fa-heart text-danger').addClass(iconClass);
+                    row.append($('<td></td>').append(icon));
+
                     row.append('<td class="text-start">' + titleLink + '</td>');
                     row.append('<td>' + item.authorName + '</td>');
                     row.append('<td>' + date.toLocaleDateString() + '</td>');
